b4b32677d2c44f5735bd71a184ae2539f390984e
KBOE2 <mail-an-kboe2@web.de>
2018-10-01 18:41:44 +0200

    missionId, missionType and missionStatus in Missionlist

--- fme-icons.dev.js
+++ fme-icons.dev.js
@@ -9,7 +9,10 @@
         // Icons in Missionlist
         $('.missionSideBarEntry').each(function () {
             let bma = !!$(this).text().match(I18n.t("fmeIcons.bma"));
-            console.log(bma);
+            let missionId = $(this).attr('mission_id');
+            let missionType = $(this).attr('mission_type_id');
+            let missionStatus = $(this).children().hasClass('mission_panel_green') ? "green" : $(this).children().hasClass('mission_panel_yellow') ? "yellow": "red";
+            console.log(missionStatus);
         });
     }
 })(I18n, jQuery);
