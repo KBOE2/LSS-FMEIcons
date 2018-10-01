7d8227fbc5c886168a84c3ed54867af1dc692d8d
KBOE2 <mail-an-kboe2@web.de>
2018-10-01 19:59:35 +0200

    Mal ein Test bezüglich des Icons austauschens

--- fme-icons.dev.js
+++ fme-icons.dev.js
@@ -1,6 +1,6 @@
 (function (I18n, $) {
 
-    let svg_template = '<?xml version="1.0" encoding="utf-8"?><svg xmlns="http://www.w3.org/2000/svg" height="1000.0" viewBox="0 0 1000.0 1000.0" width="1000.0" version="1.1" id="{{fmeIconId}}"><g><circle r="500" cx="50%" cy="50%" stroke="{{color}}" fill="{{color}}"/></g></svg>';
+    let svg_template = '<svg xmlns="http://www.w3.org/2000/svg" class="mission_vehicle_state" height="1000.0" viewBox="0 0 1000.0 1000.0" width="1000.0" version="1.1" id="{{id}}"><g><circle r="500" cx="50%" cy="50%" stroke="{{color}}" fill="{{color}}"/></g></svg>';
 
     I18n.translations.de.fmeIcons = {
         bma: "Brandmeldeanlage"
@@ -15,7 +15,7 @@
             let missionType = $(this).attr('mission_type_id');
             let missionStatus = $(this).children().hasClass('mission_panel_green') ? "green" : $(this).children().hasClass('mission_panel_yellow') ? "yellow": "red";
             $(this).find('.mission_vehicle_state').remove();
-            $(this).find('.col-xs-1').append(svg_template.replace(/{{color}}/g, missionStatus));
+            $(this).find('.col-xs-1').append(svg_template.replace(/{{id}}/g, "mission_vehicle_state_" + missionId).replace(/{{color}}/g, missionStatus));
         });
     }
 })(I18n, jQuery);
