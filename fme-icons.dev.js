d9d9ceac7f3c362674485ca486325bd476e90562
KBOE2 <mail-an-kboe2@web.de>
2018-10-01 19:57:32 +0200

    Mal ein Test bezüglich des Icons austauschens

--- fme-icons.dev.js
+++ fme-icons.dev.js
@@ -1,5 +1,7 @@
 (function (I18n, $) {
 
+    let svg_template = '<?xml version="1.0" encoding="utf-8"?><svg xmlns="http://www.w3.org/2000/svg" height="1000.0" viewBox="0 0 1000.0 1000.0" width="1000.0" version="1.1" id="{{fmeIconId}}"><g><circle r="500" cx="50%" cy="50%" stroke="{{color}}" fill="{{color}}"/></g></svg>';
+
     I18n.translations.de.fmeIcons = {
         bma: "Brandmeldeanlage"
     };
@@ -12,7 +14,8 @@
             let missionId = $(this).attr('mission_id');
             let missionType = $(this).attr('mission_type_id');
             let missionStatus = $(this).children().hasClass('mission_panel_green') ? "green" : $(this).children().hasClass('mission_panel_yellow') ? "yellow": "red";
-            console.log(missionStatus);
+            $(this).find('.mission_vehicle_state').remove();
+            $(this).find('.col-xs-1').append(svg_template.replace(/{{color}}/g, missionStatus));
         });
     }
 })(I18n, jQuery);
