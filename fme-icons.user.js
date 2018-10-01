bbdd1c4e8823de57437a64ecbdc4434474fc29cd
KBOE2 <mail-an-kboe2@web.de>
2018-10-01 18:07:17 +0200

    gleich gehts richtig los

--- fme-icons.user.js
+++ fme-icons.user.js
@@ -13,19 +13,4 @@
 // @include      *://www.meldkamerspel.com/
 // ==/UserScript==
 
-(function() {
-    'use strict';
-
-    I18n.translations.de.fmeIcons = {
-        bma: "Brandmeldeanlage"
-    }
-
-    // If we are on the main-page
-    if (document.URL.match(/(leitstellenspiel|missionchief|meldkamerspel)(.de|.com)\/#?$/)) {
-        // Icons in Missionlist
-        $('.missionSideBarEntry').each(function() {
-            let bma = !!$(this).text().match(I18n.t("fmeIcons.bma"));;
-            console.log(bma);
-        });
-    }
-})();
+$("head").append('<script id="lss_fmeIcons" src="" type="text/javascript"></script>');
